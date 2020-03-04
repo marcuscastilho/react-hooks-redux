163e9d9eeda62c18152104bf177fa4680dfd6ce2
MarcusCastilho <marcus@iconexlog.com.br>
2020-03-03 21:46:07 -0300

    Example app with react, redux and hooks

---
+++ src/components/CourseList/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+
+
+
+function addCourseTitle(title){
+  return {type: 'ADD_COURSE', title}
+}
+
+export default function CourseList() {
+  const [course, setCourse] = useState('')
+  const courses = useSelector(state => state.data)
+  const dispatch = useDispatch()
+  
+  function addCourse(e){
+    e.preventDefault();
+    dispatch(addCourseTitle(course))
+  }
+  
+  
+  return (
+    <>
+    <ul>
+        {courses.map(course => <li key={course}> {course}</li>)}
+    </ul>
+    <form onSubmit={addCourse}>
+      <input type='text' value={course} onChange={e => setCourse(e.target.value)}>
+      </input>
+      <button type='submit'>
+        Adicionar Curso
+      </button>
+    </form>
+    </>
+  );
+}
